abcb9b6ab7a5fe8e38e247c0199f345446fd75fb
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2023-01-31 01:06:10 +0530

    Added mongoose DB

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT
+const uri = process.env.URI
 
 const items = []
 const workItems = []
@@ -13,16 +15,46 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
+mongoose.connect(uri)
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+})
+
+const Item = mongoose.model('Item', itemsSchema)
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3]
+
+Item.insertMany(defaultItems, (err) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log("Successfully saved default items to DB.")
+    }
+})
+
 app.get('/', (req, res) => {
     
-    const options = {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric'
-    }
-    const today = new Date()
+    // const options = {
+    //     weekday: 'long',
+    //     month: 'long',
+    //     day: 'numeric'
+    // }
+    // const today = new Date()
 
-    const day = today.toLocaleDateString('en-US', options)
+    // const day = today.toLocaleDateString('en-US', options)
 
     res.render("list", { listTitle: day, newListItem: items })
 })
